6df85bb1a8d3d2355c6808205230a14ea23fd0be
Flint2 <gjoandet@gmail.com>
2012-11-22 13:04:54 +0000

    Modular gestures scripts designed by Tungstwenty http://forum.xda-developers.com/showpost.php?p=34164273&postcount=17

---
+++ KernelGesturesBuilder/etc/gestures/gesture-2.sh
@@ -0,0 +1,2 @@
+		# Power down the screen
+		key=26; service call window 12 i32 1 i32 1 i32 5 i32 0 i32 0 i32 $key i32 0 i32 0 i32 0 i32 8 i32 0 i32 0 i32 0 i32 0; service call window 12 i32 1 i32 1 i32 5 i32 0 i32 1 i32 $key i32 0 i32 0 i32 27 i32 8 i32 0 i32 0 i32 0 i32 0
---
+++ KernelGesturesBuilder/etc/gestures/gesture-3.sh
@@ -0,0 +1,2 @@
+	# Start the extweaks
+	am start -a android.intent.action.MAIN -n com.darekxan.extweaks.app/.ExTweaksActivity
