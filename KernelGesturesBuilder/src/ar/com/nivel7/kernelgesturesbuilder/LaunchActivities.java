4ebda61373554db49d7e86acea2554a9308d8e30
Flint2 <gjoandet@gmail.com>
2013-04-19 21:40:43 -0300

    Bump Version to 1.12

--- KernelGesturesBuilder/src/ar/com/nivel7/kernelgesturesbuilder/LaunchActivities.java
+++ KernelGesturesBuilder/src/ar/com/nivel7/kernelgesturesbuilder/LaunchActivities.java
@@ -35,7 +35,6 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
