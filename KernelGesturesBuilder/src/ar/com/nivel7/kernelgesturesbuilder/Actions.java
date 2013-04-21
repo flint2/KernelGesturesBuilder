2b8b710ed01ae8f8867dd996a5e6ce4ea23b9c77
flint2 <gjoandet@gmail.com>
2013-04-21 09:58:47 -0300

    OPEN - task 7: Add actions http://github.com/Flint2/KernelGesturesBuilder/issues/issue/7

--- KernelGesturesBuilder/src/ar/com/nivel7/kernelgesturesbuilder/Actions.java
+++ KernelGesturesBuilder/src/ar/com/nivel7/kernelgesturesbuilder/Actions.java
@@ -1,22 +1,64 @@
 package ar.com.nivel7.kernelgesturesbuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.analytics.tracking.android.EasyTracker;
 
 import android.app.ListActivity;
 import android.os.Bundle;
-public class Actions extends ListActivity {
+import android.widget.ListView;
+public class Actions extends ListActivity /* implements
+OnItemClickListener */ {
 
-	  @Override
+	public static final String[] titles = new String[] { "Strawberry",
+        "Banana", "Orange", "Mixed" };
+
+public static final String[] descriptions = new String[] {
+        "It is an aggregate accessory fruit",
+        "It is the largest herbaceous flowering plant", "Citrus Fruit",
+        "Mixed Fruits" };
+
+ public static final Integer[] images = { /* R.drawable.straw,
+        R.drawable.banana, R.drawable.orange, R.drawable.mixed */ };
+        
+
+	ListView listView;
+    List<ActionsRowItem> rowItems;
+	  
+      @Override
 	  public void onCreate(Bundle savedInstanceState) {
 		  
 	    super.onCreate(savedInstanceState);
 
 	    setContentView(R.layout.actions);
 	    
-	    
+	    rowItems = new ArrayList<ActionsRowItem>();
+        for (int i = 0; i < titles.length; i++) {
+            ActionsRowItem item = new ActionsRowItem(images[i], titles[i], descriptions[i]);
+            rowItems.add(item);
+        }
+ 
+        listView = (ListView) findViewById(R.id.list);
+        CustomListViewAdapter adapter = new CustomListViewAdapter(this,
+                R.layout.actionsrow , rowItems);
+        listView.setAdapter(adapter);
+//        listView.setOnItemClickListener(this);
 	    
 	  }
-	  
+      
+      /*
+      @Override
+      public void onItemClick(AdapterView<?> parent, View view, int position,
+              long id) {
+          Toast toast = Toast.makeText(getApplicationContext(),
+              "Item " + (position + 1) + ": " + rowItems.get(position),
+              Toast.LENGTH_SHORT);
+          toast.setGravity(Gravity.BOTTOM|Gravity.CENTER_HORIZONTAL, 0, 0);
+          toast.show();
+      }
+      */
+      
 	  @Override
 	  public void onStart() {
 	    super.onStart();
