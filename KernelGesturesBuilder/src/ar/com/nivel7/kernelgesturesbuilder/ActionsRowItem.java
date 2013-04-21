99ad2617e67931d7d32e6ef819509953e697dfac
flint2 <gjoandet@gmail.com>
2013-04-21 00:09:30 -0300

    OPEN - task 7: Add actions http://github.com/Flint2/KernelGesturesBuilder/issues/issue/7

---
+++ KernelGesturesBuilder/src/ar/com/nivel7/kernelgesturesbuilder/ActionsRowItem.java
@@ -0,0 +1,36 @@
+package ar.com.nivel7.kernelgesturesbuilder;
+
+public class ActionsRowItem {
+	private int imageId;
+    private String title;
+    private String desc;
+ 
+    public ActionsRowItem(int imageId, String title, String desc) {
+        this.imageId = imageId;
+        this.title = title;
+        this.desc = desc;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+    public void setImageId(int imageId) {
+        this.imageId = imageId;
+    }
+    public String getDesc() {
+        return desc;
+    }
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    @Override
+    public String toString() {
+        return title + "\n" + desc;
+    }
+}
+
